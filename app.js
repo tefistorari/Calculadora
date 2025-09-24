3f98e68706a9a09799587043f35b79b72b12007e
tefistorari <estefaniastorari14@gmail.com>
2025-09-24 18:44:47 -0300

    trycatch-eval

--- app.js
+++ app.js
@@ -9,5 +9,9 @@ function clearDisplay(){
 }
 
 function calculate(){
-    display.value = eval(display.value);
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
 }
